d68f8cf375b959c6f2610aa6642ba0d1911f50f0
Ugur Akin <ugurakinsoftware@gmail.com>
2021-02-01 11:15:28 +0300

    About page content is filled with a draft.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -35,6 +35,12 @@ const useStyles = createUseStyles({
       width: 0,
       height: 0,
     },
+    '& > section > p': {
+      marginTop: 0,
+      boxSizing: 'border-box',
+      paddingLeft: '2rem',
+      fontSize: '80%',
+    },
   },
   avatarContainer: {
     display: 'inline-block',
@@ -112,29 +118,97 @@ const About = () => {
               Aug 2014 - Feb 2020
             </span>{' '}
           </p>
-          <p
-            style={{
-              marginTop: 0,
-              boxSizing: 'border-box',
-              paddingLeft: '2rem',
-              fontSize: '80%',
-            }}
-          >
+          <p>
             <b>Major:</b> Computing Science <br />
             <b>Minor:</b> Mathematics
           </p>
         </section>
         <section>
-          <h4>Programming History</h4>
+          <h4>Why Programming?</h4>
+          <p>
+            I started programming when I was 16. A friend who was experienced in
+            programming showed me the fundamentals of{' '}
+            <code>Visual Basic 6</code> and we went over how to make form
+            applications. I loved it so much that I spent most of my summer
+            staying inside and programming ridiculous and barely-functioning
+            apps such as calculators and stopwatches.{' '}
+          </p>
+          {/** TODO(Ugur): Emoticon for smiley. */}
+          <p>
+            But the first program I was proud of making, was a game. It looked
+            really silly, but it was kind of fun! I made everyone in my family
+            play it, and they were both impressed and mildly annoyed :). It
+            looked like this:
+          </p>
+          {/* <TrainGame /> */}
+          <p>
+            I placed several <code>TextBox</code> form controls back-to-back and
+            filled one with <code>'|'</code> characters. When players hit{' '}
+            <code>Start</code>, the <code>'|'</code> characters would start
+            moving from one <code>TextBox</code> to another and that was
+            supposed to simulate a moving train. The goal was to stop the train
+            by the marker placed under one of the middle{' '}
+            <code>TextBox Control</code>.
+          </p>
+          {/** TODO(Ugur): Link to periodic table. */}
+          <p>
+            I was always drawn to programming &mdash; thanks Hollywood for all
+            the "Hacker" portrayals. But after that summer, I finally understood
+            what programming really was and I liked it now, more than before. I
+            continued to study programming on my own throughout high-school.
+            Eventually made a periodic table app for one of my year projects, it
+            was inspired by this website.
+          </p>
+          {/**TODO(Ugur): Links to websites, books, etc. */}
+          <p>
+            To this day, I continue to study programming with the same passion
+            and eagerness. After graduating, I learned ReactJS from scratch (and
+            still learning) with Pluralsight courses and individual projects, I
+            often hop onto <code>LeetCode</code>, solve some coding problems,
+            learn about new algorithms or new ways to use common
+            data-structures, I read about UI/UX on the web, I especially read
+            articles on CSS-Tricks to broaden my perspective and understanding
+            of design and styling, I'm a big fan of modern <code>c++</code> so
+            I've been reading Scott Meyers' renowned Effective Modern
+            C++&hellip;
+          </p>
+          <p>
+            Within this field, there is always something more to learn; whether
+            it is a new technology or a detail about an existing one. And while
+            I don't think it's necessary (or possible) to know everything, I
+            think it's the number one sign of a passionate programmer to keep up
+            with the changes and updating one's stale methods and knowledge.
+          </p>
         </section>
-        <section>
+        {/* <section>
           <h4>Philosophies</h4>
-        </section>
-        <section>
+        </section> */}
+        {/* <section>
           <h4>Fields of Interest</h4>
-        </section>
+        </section> */}
         <section>
           <h4>Hobbies</h4>
+          <p>
+            My hobbies are a big part of who I am; I believe having hobbies is
+            important because they often come with useful transferable skills. I
+            love playing sports – exercising not only helps keeping healthy, but
+            I find also increases productivity throughout the day. I played
+            several different branches like soccer and swimming, but I pursued
+            tennis especially my whole life. I played competitively as a kid and
+            in college, and I continue to play regularly.{' '}
+          </p>
+          <p>
+            Furthermore, I’m passionate about music; just listening to it,
+            discovering new songs, or even producing here and there. I love
+            playing instruments as well – I want to master more instruments one
+            day but currently, I play the piano and practice regularly with an
+            instructor.
+          </p>
+          <p>
+            Apart from that, you can often find me solving coding challenges,
+            studying French, playing chess or video games, and occasionally
+            reading books in my free time.
+          </p>
         </section>
       </article>
     </section>
